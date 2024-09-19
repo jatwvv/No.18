50a8b0bcd1bbe251ac3a0bd047f9459b867c68c2
jatwvv <146978372+jatwvv@users.noreply.github.com>
2024-09-19 19:34:38 +0300

    idk

---
+++ script.js
@@ -0,0 +1,13 @@
+let persentageInput = document.getElementById("persentage");
+let euroInput = document.getElementById("euro");
+let yearInput = document.getElementById("year");
+let radioOne = document.getElementById("radio1");
+let radioTwo = document.getElementById("radio2");
+let calcBtn = document.getElementById("calc");
+
+
+let persentageInputValue = persentageInput.value;
+let euroInputValue = euroInput.value;
+let yearInputValue = yearInput.value;
+let radioOneValue = radioOne.value;
+let radioTwoValue = radioTwo.value;
